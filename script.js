e1338737140c45544848656d34b1cc5c00175537
Mark LaBarbara <mlabarbara@gmail.com>
2023-05-06 14:00:41 -0600

    increase animation distance

--- script.js
+++ script.js
@@ -9,25 +9,27 @@ window.addEventListener('DOMContentLoaded', function() {
         delay: 200,
         duration: 1000,
         distance: '200px',
-        origin: 'bottom',
+        origin: 'top',
+        reset: true,
         easing: 'ease-in-out',
         interval: 100,
         opacity: 0,
         scale: 0.9,
         viewFactor: 0.3,
-        mobile: false
+        mobile: true
     });
 
     ScrollReveal().reveal('.image', {
         delay: 200,
         duration: 1000,
         distance: '200px',
-        origin: 'bottom',
+        origin: 'top',
+        reset: true,
         easing: 'ease-in-out',
         interval: 100,
         opacity: 0,
         scale: 0.9,
         viewFactor: 0.3,
-        mobile: false
+        mobile: true
     });
 });
