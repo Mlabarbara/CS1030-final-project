7872978424bd05cefa6340f0f4228eb7e051679e
Mark LaBarbara <mlabarbara@gmail.com>
2023-05-06 13:36:44 -0600

    script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+
+// Initialize ScrollReveal
+ScrollReveal().reveal('.container', {
+    delay: 200,
+    duration: 1000,
+    distance: '20px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    interval: 100
+});
+
+ScrollReveal().reveal('.image', {
+    delay: 200,
+    duration: 1000,
+    distance: '20px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    interval: 100
+});
