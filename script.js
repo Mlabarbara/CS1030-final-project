6bfc9ead3f13ea010aebd8d9b0c84ddcfe273a6d
Mark LaBarbara <mlabarbara@gmail.com>
2023-05-06 14:06:05 -0600

    messing with the parallex effect

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-// script.js
-
-// Initialize ScrollReveal
-// script.js
 window.addEventListener('DOMContentLoaded', function() {
     ScrollReveal().reveal('.container', {
         delay: 200,
         duration: 1000,
-        distance: '200px',
-        origin: 'top',
+        distance: '0',
+        origin: 'left',
         reset: true,
         easing: 'ease-in-out',
         interval: 100,
         opacity: 0,
-        scale: 0.9,
+        scale: 1,
         viewFactor: 0.3,
         mobile: true,
-        container: document.querySelector('.container') // Specify the container element for the parallax effect
+        beforeReveal: function (domEl) {
+            ScrollReveal().container.style.overflowX = 'hidden'; // Hide horizontal scrollbar during the animation
+            ScrollReveal().container.style.overflowY = 'hidden'; // Hide vertical scrollbar during the animation
+            domEl.style.transform = 'translateX(-50px)'; // Initial position of the element
+        },
+        afterReveal: function (domEl) {
+            domEl.style.transform = 'translateX(50px)'; // Final position of the element after the animation
+        },
+        beforeReset: function (domEl) {
+            domEl.style.transform = 'translateX(-50px)'; // Reset the position of the element before hiding it
+        },
+        afterReset: function (domEl) {
+            domEl.style.transform = ''; // Clear the transform style after hiding the element
+            ScrollReveal().container.style.overflowX = ''; // Restore the overflow property of the container
+            ScrollReveal().container.style.overflowY = ''; // Restore the overflow property of the container
+        }
     });
 
     ScrollReveal().reveal('.image', {
         delay: 200,
         duration: 1000,
-        distance: '200px',
-        origin: 'top',
+        distance: '0',
+        origin: 'left',
         reset: true,
         easing: 'ease-in-out',
         interval: 100,
         opacity: 0,
-        scale: 0.9,
+        scale: 1,
         viewFactor: 0.3,
         mobile: true,
-        container: document.querySelector('.container') // Specify the container element for the parallax effect
+        beforeReveal: function (domEl) {
+            ScrollReveal().container.style.overflowX = 'hidden'; // Hide horizontal scrollbar during the animation
+            ScrollReveal().container.style.overflowY = 'hidden'; // Hide vertical scrollbar during the animation
+            domEl.style.transform = 'translateX(-50px)'; // Initial position of the element
+        },
+        afterReveal: function (domEl) {
+            domEl.style.transform = 'translateX(50px)'; // Final position of the element after the animation
+        },
+        beforeReset: function (domEl) {
+            domEl.style.transform = 'translateX(-50px)'; // Reset the position of the element before hiding it
+        },
+        afterReset: function (domEl) {
+            domEl.style.transform = ''; // Clear the transform style after hiding the element
+            ScrollReveal().container.style.overflowX = ''; // Restore the overflow property of the container
+            ScrollReveal().container.style.overflowY = ''; // Restore the overflow property of the container
+        }
     });
 });
-// script.js
