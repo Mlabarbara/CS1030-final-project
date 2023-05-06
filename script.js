99e55585a29798bc3601a825583455c975798446
Mark LaBarbara <mlabarbara@gmail.com>
2023-05-06 14:09:35 -0600

    swiched it up

--- script.js
+++ script.js
@@ -1,61 +1,19 @@
-window.addEventListener('DOMContentLoaded', function() {
-    ScrollReveal().reveal('.container', {
-        delay: 200,
-        duration: 1000,
-        distance: '0',
-        origin: 'left',
-        reset: true,
-        easing: 'ease-in-out',
-        interval: 100,
-        opacity: 0,
-        scale: 1,
-        viewFactor: 0.3,
-        mobile: true,
-        beforeReveal: function (domEl) {
-            ScrollReveal().container.style.overflowX = 'hidden'; // Hide horizontal scrollbar during the animation
-            ScrollReveal().container.style.overflowY = 'hidden'; // Hide vertical scrollbar during the animation
-            domEl.style.transform = 'translateX(-50px)'; // Initial position of the element
-        },
-        afterReveal: function (domEl) {
-            domEl.style.transform = 'translateX(50px)'; // Final position of the element after the animation
-        },
-        beforeReset: function (domEl) {
-            domEl.style.transform = 'translateX(-50px)'; // Reset the position of the element before hiding it
-        },
-        afterReset: function (domEl) {
-            domEl.style.transform = ''; // Clear the transform style after hiding the element
-            ScrollReveal().container.style.overflowX = ''; // Restore the overflow property of the container
-            ScrollReveal().container.style.overflowY = ''; // Restore the overflow property of the container
-        }
-    });
+// Initialize ScrollMagic controller
+var controller = new ScrollMagic.Controller();
 
-    ScrollReveal().reveal('.image', {
-        delay: 200,
-        duration: 1000,
-        distance: '0',
-        origin: 'left',
-        reset: true,
-        easing: 'ease-in-out',
-        interval: 100,
-        opacity: 0,
-        scale: 1,
-        viewFactor: 0.3,
-        mobile: true,
-        beforeReveal: function (domEl) {
-            ScrollReveal().container.style.overflowX = 'hidden'; // Hide horizontal scrollbar during the animation
-            ScrollReveal().container.style.overflowY = 'hidden'; // Hide vertical scrollbar during the animation
-            domEl.style.transform = 'translateX(-50px)'; // Initial position of the element
-        },
-        afterReveal: function (domEl) {
-            domEl.style.transform = 'translateX(50px)'; // Final position of the element after the animation
-        },
-        beforeReset: function (domEl) {
-            domEl.style.transform = 'translateX(-50px)'; // Reset the position of the element before hiding it
-        },
-        afterReset: function (domEl) {
-            domEl.style.transform = ''; // Clear the transform style after hiding the element
-            ScrollReveal().container.style.overflowX = ''; // Restore the overflow property of the container
-            ScrollReveal().container.style.overflowY = ''; // Restore the overflow property of the container
-        }
-    });
-});
+// Define the tween for the horizontal motion effect
+var horizontalTween = gsap.timeline()
+    .to('.container', { x: '-50%', ease: 'power1.inOut' })
+    .to('.image', { x: '-50%', ease: 'power1.inOut' }, '-=1'); // Start the image animation 1 second before the container animation
+
+// Create a ScrollMagic scene for the horizontal motion effect
+var horizontalScene = new ScrollMagic.Scene({
+    triggerElement: '.container',
+    triggerHook: 'onEnter',
+    duration: '200%' // Adjust the duration as needed
+})
+.setTween(horizontalTween)
+.addTo(controller);
+
+// Add debug indicators (optional)
+horizontalScene.addIndicators({ name: 'Horizontal Motion' });
